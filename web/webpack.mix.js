00d2cf661b7df8ae375aa3988cab47d28d7b5171
kkloke <kokkeong.loke@modefair.com>
2018-01-17 07:49:46 +0800

    Add migrate and auth.

--- web/webpack.mix.js
+++ web/webpack.mix.js
@@ -14,4 +14,4 @@ let mix = require('laravel-mix');
 mix.react('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
-mix.copyDirectory('resources/themes/paperkit2', 'public/themes/paperkit2');
+mix.copyDirectory('resources/assets/themes/paperkit2', 'public/themes/paperkit2');
